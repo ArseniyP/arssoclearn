808a5060ba75d2c52f96bb1d8a1b276d3a828323
Арсений <arseniyp91@gmail.com>
2014-04-06 10:54:20 +0300

    Страница входа пользователя
    На регистрации только заглушка. Архитектура сайта соц. сети еще не
    продуманна.

---
+++ index.php
@@ -0,0 +1,23 @@
+﻿<a href="./users.txt">Просмотреть список зарегистрированных пользователей(тестовая версия)</a><br/>
+<form method='post' action='./profile.php'>
+	<table>
+		<tr>
+			<td>Логин(ник)</td>
+			<td><input type='text' name='login'></td>
+		</tr>
+		<tr>
+			<td>Пароль</td>
+			<td><input type='password' name='password'></td>
+		</tr>
+		<tr>
+			<td><input type='submit' value="Войти"></td>
+			<td>&nbsp;</td>
+		</tr>
+	</table>
+</form>
+
+<form method='post' action='./registration.php'>
+<table><tr><td>
+<input type='submit' value="Регистрация">
+</td></tr></table>
+</form>
---
+++ profile.php
@@ -0,0 +1,55 @@
+﻿<?php
+	//Проверка на пустые параметры
+	$incorrectData = false;
+	if(isset($_POST["login"]) && empty($_POST["login"]))
+	{
+		echo "Не введен логин<br/>";
+		$incorrectData = true;
+	}
+	if(isset($_POST["password"]) && empty($_POST["password"]))
+	{
+		echo "Не введен пароль<br/>";
+		$incorrectData = true;
+	}
+	
+	if($incorrectData)
+	{
+		echo "<a href='./index.php'>Повторить вход</a>";
+		exit();
+	}
+	
+	$Users;
+	$usersRead = file("./users.txt", FILE_IGNORE_NEW_LINES);
+	if($usersRead == false)
+	{
+		echo "Не удалось открыть файл users.txt";
+		exit();
+	}
+	
+	foreach($usersRead as $line)
+	{
+		$logAndPass = explode(' ', $line);
+		$Users[] = Array("login" => $logAndPass[0], "password" => $logAndPass[1]);
+	}
+	
+	$user;
+	$isSearch = false;
+	foreach($Users as $sUser)
+	{
+		if(strcmp($sUser["login"], $_POST["login"]) === 0)
+		{
+			$isSearch = true;
+			$user = $sUser;
+		}
+	}
+	
+	if($isSearch)
+	{
+		if(strcmp($user["password"], $_POST["password"]) === 0)
+			echo "Пользователь ".$user["login"]." авторизирован.";
+		else
+			echo "Введен неверный пароль";
+	}
+	else
+		echo "Пользователь ".$_POST["login"]." не зарегистирован";
+?>
